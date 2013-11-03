4587b0086fb894f0dfcbd2e01f906e58be68ce6a
James Barbosa <jamesgbarbosa@gmail.com>
2013-11-03 13:07:29 +0800

    Added Amendment types ref table

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,6 +2,7 @@ import com.dataentry.Agent
 import com.dataentry.Beneficiary
 import com.dataentry.Planholder
 import com.dateentry.reftables.Product
+import com.dateentry.reftables.AmendmentTypes
 
 class BootStrap {
 
@@ -12,6 +13,21 @@ class BootStrap {
         def product3 = new Product(name: 'Sample4').save(flush: true, failOnError: true)
         def product4 = new Product(name: 'Sample5').save(flush: true, failOnError: true)
         def product5 = new Product(name: 'Sample6').save(flush: true, failOnError: true)
+
+        new AmendmentTypes(name: 'Transfer').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Change Name').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Change Address').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Change Beneficiary').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Reinstatement').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Plan Upgrade').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Change Payment Mode').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Replacement Policy Contract').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Replacement CFP').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Initial CFP Request').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Replacement Policy Contract / CFP').save(flush: true, failOnError: true)
+        new AmendmentTypes(name: 'Update Birthdate').save(flush: true, failOnError: true)
+
+
 
         new Agent(firstName: 'Agent1', lastName: 'test' , birthdate: new Date(), gender: 'Male', clientType: 'Agent').save(flush: true, failOnError: true)
         new Agent(firstName: 'Agent2', lastName: 'apple' , birthdate: new Date(), gender: 'Male', clientType: 'Agent').save(flush: true, failOnError: true)
--- grails-app/taglib/com/dateentry/SelectTagLib.groovy
+++ grails-app/taglib/com/dateentry/SelectTagLib.groovy
@@ -1,6 +1,7 @@
 package com.dateentry
 
 import com.dateentry.reftables.Product
+import com.dateentry.reftables.AmendmentTypes
 
 class SelectTagLib {
 
@@ -9,6 +10,12 @@ class SelectTagLib {
     void product = { attrs ->
 
         out << g.select(name: 'product', id: 'product' , from: Product.list(), optionKey: 'name', optionValue: 'name', value: attrs.value)
+
+    }
+
+    void amendmentTypes = { attrs ->
+
+        out << g.select(name: 'amendmentType', id: 'amendmentType' , from: AmendmentTypes.list(), optionKey: 'name', optionValue: 'name', value: attrs.value)
 
     }
 
