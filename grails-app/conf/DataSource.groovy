e62b517418d841c7fea30931b7863a3c52c06dc0
James Barbosa <jamesgbarbosa@gmail.com>
2013-12-08 17:50:43 +0800

    Updated datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -12,13 +12,13 @@ hibernate {
 environments {
     development {
         dataSource {
-//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:postgresql://localhost:5432/dataentry"
-            username = "postgres"
-            password = "postgres"
-            dialect = com.dataentry.TableNamePostgresDialect
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+//            url = "jdbc:postgresql://localhost:5432/dataentry"
+//            username = "postgres"
+//            password = "postgres"
+//            dialect = com.dataentry.TableNamePostgresDialect
 //            url = "jdbc:postgresql://localhost:5432/dataentry"
 //            username = "postgres"
 //            password = "postgres"
